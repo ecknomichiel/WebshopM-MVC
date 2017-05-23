02abc7df3a087e3d0be8e9af2e88bfe029455373
ecknomichiel <michiel@frostvikenit.eu>
2017-05-23 12:55:14 +0200

    Initial project with first model

---
+++ WebshopM-MVC/Models/ShopItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebshopM_MVC.Models
+{
+    public class ShopItem
+    {
+        [key]
+        public int ArticleNumber { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ShelfPosition { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+    }
+}

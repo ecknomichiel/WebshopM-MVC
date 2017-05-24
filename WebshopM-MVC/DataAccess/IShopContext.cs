f68023ad23132a4aa8ef35683fa5af56d08ba72f
ecknomichiel <michiel@frostvikenit.eu>
2017-05-24 15:26:21 +0200

    Included ignored files

---
+++ WebshopM-MVC/DataAccess/IShopContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using WebshopM_MVC.Models;
+
+namespace WebshopM_MVC.DataAccess
+{
+    interface IShopContext
+    {
+        ShopItem Get(int aId);
+        void Put(ShopItem aItem);
+        void Delete(ShopItem aItem);
+        void Save(IEnumerable<ShopItem> items);
+
+        IEnumerable<ShopItem> GetAllItems();
+    }
+
+    static class ShopContextFactory
+    {
+        public static IShopContext CreateShopContext()
+        {
+            return new StoreContext();
+        }
+    }
+}
